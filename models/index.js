a4ec8339312cd2bf232e8a2a52d557982b5fb368
ripetangerine <dev.ripetangerine@gmail.com>
2026-03-17 17:15:03 +0900

    config: sequelize model setting

--- models/index.js
+++ models/index.js
@@ -1,23 +1,28 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+import { readdirSync } from 'fs';
+import { basename as _basename, join } from 'path';
+import Sequelize, { DataTypes } from 'sequelize';
+import { env as _env } from 'process';
+import path from "node:path";
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const basename = _basename(__filename);
+const env = _env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(_env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-  .readdirSync(__dirname)
+readdirSync(__dirname)
   .filter(file => {
     return (
       file.indexOf('.') !== 0 &&
@@ -27,7 +32,7 @@ fs
     );
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = require(join(__dirname, file))(sequelize, DataTypes);
     db[model.name] = model;
   });
 
@@ -40,4 +45,7 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+// db.Article = require('./article.js')(sequelize,Sequelize);
+// 오류 가능성
+
+export default db;
